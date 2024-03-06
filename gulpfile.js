86a28470e4fe2343423d5ff460417ae0288d6b33
Daisy Llivisaca <daisynohelia@gmail.com>
2024-03-05 21:04:37 -0500

    actualizado hasta pag 170 del pdf Proyecto 7

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,63 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require('gulp-plumber');
+
+//IMÁGENES
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     
-    src('src/scss/app.scss') //Identificar el archivo de SASS
+    src('src/scss/**/*.scss') //Identificar el archivo de SASS
+        .pipe( plumber() )
         .pipe( sass() ) 
         .pipe( dest("build/css") ) //Almacenar en el disco duro
 
     done();
 }
 
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+
+    done();
+}
+
+function versionWebp ( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionAvif ( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
 function dev(done) {
-    watch('src/scss/app.scss',css)
+    watch('src/scss/**/*.scss',css)
 
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel ( imagenes, versionWebp, versionAvif, dev );
